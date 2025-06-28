4d0993814c73aa1fadf96ffa878ce4239fb7de14
nefigarcia <bluedivee@hotmail.com>
2025-06-27 21:13:46 -0600

    transforming countries names code 2

--- ETL_functions.py
+++ ETL_functions.py
@@ -2,11 +2,25 @@ import csv
 open_file = 'jobs_postings.csv'
 
 def transform_data(data):
+    country_map = {
+        'IN': 'India',
+        'US': 'United States',
+        'GB': 'United Kingdom',
+        'CA': 'Canada',
+        'DE': 'Germany',
+        'FR': 'France',
+        # Add more as needed
+    }
     transformed_data = []
     for entry in data:
 
         if len(entry) > 2 and '\n' in entry[2]:
             level,location = entry[2].split('\n', 1)
+            if ',' in location:
+                parts = location.split(',')
+                country_code = parts[-1].strip()
+                country_full = country_map.get(country_code, country_code)
+                location = country_full
             transformed_data.append([entry[0], entry[1], location, level])
         else:
             transformed_data.append(entry)
